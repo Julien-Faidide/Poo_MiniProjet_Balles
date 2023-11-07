a9721acd54272a0fd770c00ed0c6d4c1588590f6
katastorm <45739730+katastorm@users.noreply.github.com>
2023-11-07 14:21:41 +0100

    consignes

---
+++ Programme-de-depart.py
@@ -0,0 +1,33 @@
+import pygame, sys
+import time
+from pygame.locals import *
+
+
+
+pygame.display.init()
+fenetre = pygame.display.set_mode((640, 480))
+fenetre.fill([0,0,0])
+
+x = 300
+y = 200
+dx = 4
+dy = -3
+couleur = (45,170,250)
+
+while True :
+    fenetre.fill([0,0,0])
+    pygame.draw.circle(fenetre,couleur,(x,y),10)
+
+    x += dx
+    y += dy
+
+    pygame.display.update()
+
+    # routine pour pouvoir fermer «proprement» la fenêtre Pygame
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.display.quit()
+            sys.exit()
+
+
+    time.sleep(0.1)
